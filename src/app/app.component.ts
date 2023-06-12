9450b3c4f03c2cf99045026c602212cf668be231
Михаил Воронин <voronok309@mail.ru>
2023-06-13 04:58:11 +0700

    прилелиз

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'WeddingDay';
+
+  catalogItems: Array<any>;
+  serviceItems: Array<any>;
+
+  ngOnInit(): void {
+    this.catalogItems = [
+      {
+        img: '../assets/images/item_1.jpg',
+        number: 4762,
+        price: 25400
+      },
+      {
+        img: '../assets/images/item_2.jpg',
+        number: 1906,
+        price: 30900
+      },
+      {
+        img: '../assets/images/item_3.jpg',
+        number: 3512,
+        price: 18500
+      },
+      {
+        img: '../assets/images/item_4.jpg',
+        number: 4115,
+        price: 20400
+      },
+      {
+        img: '../assets/images/item_5.jpg',
+        number: 4015,
+        price: 18300
+      },
+      {
+        img: '../assets/images/item_6.jpg',
+        number: 1475,
+        price: 22700
+      },
+      {
+        img: '../assets/images/item_7.jpg',
+        number: 2060,
+        price: 34300
+      },
+      {
+        img: '../assets/images/item_8.jpg',
+        number: 1255,
+        price: 25400
+      },
+    ];
+
+    this.serviceItems = [
+      {
+        img: '../assets/icons/service_item_1.png',
+        title: 'бесплатная подгонка по фигуре'
+      },
+      {
+        img: '../assets/icons/service_item_2.png',
+        title: 'бесплатная консультация'
+      },
+      {
+        img: '../assets/icons/service_item_3.png',
+        title: 'хранение платья до свадьбы'
+      },
+      {
+        img: '../assets/icons/service_item_4.png',
+        title: 'отпаривание бесплатно'
+      },
+    ]
+  }
 }
